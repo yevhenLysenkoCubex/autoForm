3448241f4c47f814d4f00c35f0ca7d631b8b3b55
Евгений Лысенко <evgenijlysenko@Evgenijs-MacBook-Pro.local>
2023-11-03 14:29:11 +0200

    2nd autoform

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
   const [formData, setFormData] = useState<
     Record<string, string | number | boolean | string[] | number[]>
   >({});
+  const [formData1, setFormData1] = useState<
+    Record<string, string | number | boolean | string[] | number[]>
+  >({});
 
   const forms = [
     {
@@ -21,9 +24,6 @@ function App() {
       options: ["hello", "world", "choose", "mee"],
       default_value: "Select default value",
     },
-    {
-      type: "number",
-    },
   ] as FormTypes[];
 
   const handleSubmit = (
@@ -32,10 +32,36 @@ function App() {
     setFormData(data);
   };
 
+  const forms1 = [
+    {
+      type: "text",
+      default_value: "TExt Default Value",
+    },
+    {
+      type: "textarea",
+    },
+    {
+      type: "dropdown",
+      options: ["hello", "world", "choose", "mee"],
+    },
+    {
+      type: "number",
+      default_value: 15,
+    },
+  ] as FormTypes[];
+
+  const handleSubmit1 = (
+    data: Record<string, string | number | boolean | string[] | number[]>
+  ) => {
+    setFormData1(data);
+  };
+
   return (
     <main>
       <AutoForm forms={forms} handleSubmit={handleSubmit} />
       <pre>{JSON.stringify(formData, null, 2)}</pre>
+      <AutoForm forms={forms1} handleSubmit={handleSubmit1} />
+      <pre>{JSON.stringify(formData1, null, 2)}</pre>
     </main>
   );
 }
